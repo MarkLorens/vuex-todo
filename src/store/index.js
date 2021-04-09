8f59d9adb39f4483bddddcb4f1a15bc14be1ea3c
Mark Lorens <57747268+MarkLorens@users.noreply.github.com>
2021-04-09 13:33:30 +0800

    Init

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+import todos from './modules/todos.js'
+
+export default createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    todos
+  }
+})
